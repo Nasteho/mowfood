61c31ef898d167347f344b1a3f393da653708609
Nasteho <Nasteho@users.noreply.github.com>
2018-11-16 11:57:20 +0100

    arbjeder på at sende mail fra kontakt form

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,22 +1,3 @@
-/*function initMap() {
-
-  var uluru = {
-    lat: 55.708681,
-    lng: 9.536134
-  };
-
-
-  var map = new google.maps.Map(document.getElementById('googlemaps'), {
-    zoom: 15,
-    center: uluru
-  });
-
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
-
-*/
 
 
 
