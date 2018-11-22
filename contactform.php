a41e7f025e396f0bd086c3b5cfc9948c4a4427b6
Nasteho <Nasteho@users.noreply.github.com>
2018-11-22 23:48:25 +0100

    starting english version

--- contactform.php
+++ contactform.php
@@ -1,10 +1,12 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent=" From: $name \n  \n Message: $message";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail("contact@nastehoelmi.dk", $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+if(isset($_POST['submit'])){
+    $to = "contact@nastehoelmi.dk"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = "Form submission";
+    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $headers = "From:" . $from;
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    }
 ?>
